7c6f930758f7aef744676a9fa35a3b203b3fb9d3
matthiasbenschop <matthiasbenschop@hotmail.com>
2021-04-30 15:24:31 +0200

    create vakantie

---
+++ Vakantiehuisje/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Vakantie Huisjes</title>
+</head>
+<body>
+
+<div class="topnav" id="myTopnav">
+  <a href="index.php" class="active"></a>
+  <a href="index.php">Home</a>
+  <a href="over.php">Over</a>
+  <div class="dropdown">
+    <button class="dropbtn">Vakantiehuisjes 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+  </div> 
+  <a href="contact.php">Contact</a>
+  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+</div>
+    
+    <script src="script.js"></script>
+
+</body>
+</html>
