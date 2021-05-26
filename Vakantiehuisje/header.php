36dc453265b062a0dae661f2e510876ea4847c01
matthiasbenschop <matthiasbenschop@hotmail.com>
2021-05-26 08:49:48 +0200

    start vakantiehuisjes

--- Vakantiehuisje/header.php
+++ Vakantiehuisje/header.php
@@ -6,29 +6,47 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <title>Vakantie Huisjes</title>
 </head>
 <body>
 
-<div class="topnav" id="myTopnav">
-  <a href="index.php" class="active"></a>
-  <a href="index.php">Home</a>
-  <a href="over.php">Over</a>
-  <div class="dropdown">
-    <button class="dropbtn">Vakantiehuisjes 
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content">
-      <a href="#">Link 1</a>
-      <a href="#">Link 2</a>
-      <a href="#">Link 3</a>
-    </div>
-  </div> 
-  <a href="contact.php">Contact</a>
-  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
-</div>
+<!-- Menu -->
+  
+<div class="container">
+ <h1>
+    <a class="menu-color" href="#menu">Menu</a>
+ </h1>
     
+<div class="popover" id="menu">
+ <div class = 'content'>
+  <a href="#" class="close"></a>
+   <div class = 'nav'>
+    <ul class = 'nav_list'>
+      
+      <div class = 'nav_list_item'>
+	  <li><a href="index.php">Home</a></li>
+      </div>
+    <div class = 'nav_list_item'>
+	  <li><a href="vakantie.php">Huisjes</a></li>
+      </div>
+    <div class = 'nav_list_item'>
+	  <li><a href="login.php">Inloggen</a></li>
+      </div>
+      
+	</ul>
+  </div>
+ </div>
+ </div>
+ </div>
+    
+
+
     <script src="script.js"></script>
 
 </body>
 </html>
+
---
+++ Vakantiehuisje/register.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <title>Inloggen</title>
+    <link href="style.css" rel="stylesheet">
+    <style>
+      body{
+        background: whitesmoke;
+      }
+      </style>
+</head>
+<body>
+
+    <i class="fas fa-arrow-left"></i>
+
+  <form class="box" method="post">
+  <h1>Registreren</h1>
+  <input type="text" name="username" placeholder="Gebruikersnaam">
+  <input type="password" name="password" placeholder="Wachtwoord">
+  <input type="radio" id="male" name="beheer" value="male"> <label for="male" class="radio" >&nbspSeller</label></input>
+  <input type="submit" name="" value="Registreer">
+
+</form>
+
+</body>
+</html>
